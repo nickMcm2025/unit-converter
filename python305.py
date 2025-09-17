f4f725084f825ca510d99c07c6b59a4a8540e901
nickMcm2025 <nmcmillan5167@students.vbisd.org>
2025-09-17 08:40:38 -0400

    Add files via upload

---
+++ python305.py
@@ -0,0 +1,8 @@
+a= float(input("enter a age in years"))
+print("in months thats",a*12)
+print("in days thats",a*365.25)
+print("in hours thats",a*365.25*24)
+print("in minutes thats",a*365.25*24*60)
+print("in seconds thats",a*365.25*24*60*60)
+
+      
